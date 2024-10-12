a0c831df9ea8176c867cf5df55923ec8321d30f7
wajahat ali <wajahatfaryadali@gmail.com>
2024-10-12 18:38:37 +0500

    error middleware added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,17 +1,18 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 
-export const signUpController = async (req, res) => {
+export const signUpController = async (req, res, next) => {
     const { username, email, password } = req.body;
-    
+
     const encryptedPassword = bcryptjs.hashSync(password, 10);
-    
+
     try {
-        const newUser = new User({ username, email, password : encryptedPassword });
+        const newUser = new User({ username, email, password: encryptedPassword });
         await newUser.save()
         res.status(201).json({ message: "User created Successful" });
     } catch (error) {
-        res.status(500).json({error: error.message})
+        next(error)
+        // res.status(500).json({error: error.message})
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -23,3 +23,15 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error!'
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
