e000ce55b836e46219d752e538ebad1c06a2f7c0
wajahat ali <wajahatfaryadali@gmail.com>
2024-10-12 17:59:32 +0500

    bcryptjs installed for password encryption

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
 
 export const signUpController = async (req, res) => {
+    const { username, email, password } = req.body;
+    
+    const encryptedPassword = bcryptjs.hashSync(password, 10);
+    
     try {
-        const { username, email, password } = req.body;
-
-        const newUser = new User({ username, email, password });
-
+        const newUser = new User({ username, email, password : encryptedPassword });
         await newUser.save()
-
         res.status(201).json({ message: "User created Successful" });
-
     } catch (error) {
         res.status(500).json({error: error.message})
     }
